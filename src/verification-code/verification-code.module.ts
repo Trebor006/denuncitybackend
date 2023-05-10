a8a7bfdf745b291e093ef0f01da290d04319c6e7
Robert Cabrera <clrobert.06@gmail.com>
2023-05-10 02:54:47 -0400

    feat(master): publish to dockerhub

--- src/verification-code/verification-code.module.ts
+++ src/verification-code/verification-code.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { VerificationCodeService } from './verification-code.service';
 import { VerificationCodeController } from './verification-code.controller';
 import { CodeVerifierService } from '../code-verifier/code-verifier.service';
+import {CodeVerifierModule} from "../code-verifier/code-verifier.module";
+import {MongooseModule} from "@nestjs/mongoose";
+import {User, UserSchema} from "../schemas/user.schema";
+import {MailVerifier, MailVerifierSchema} from "../schemas/mail.verifier.schema";
+import {PasswordHistory, PasswordHistorySchema} from "../schemas/password.history.schema";
 
 @Module({
+  imports: [
+    CodeVerifierModule,
+    MongooseModule.forFeature([
+      { name: MailVerifier.name, schema: MailVerifierSchema },
+    ]),
+  ],
   controllers: [VerificationCodeController],
   providers: [VerificationCodeService, CodeVerifierService],
 })
